9c3e8fbb7fee3faa3a2e739184e789899276a305
Marilena Niemann <mari_niemann@web.de>
2019-11-06 15:33:24 +0100

    excercise show hide

--- src/main.js
+++ src/main.js
@@ -1,15 +1,35 @@
-console.log('it works')
-
 fetch('http://localhost:3333/cards') // 2 Promisses: then und catch
   .then(res => res.json())
   //.then(cards => console.log(cards))
-  .then(cards => cards.forEach(showLinks)) // fetch: macht einen request an den Server 3333 cards
+  .then(cards => cards.forEach(showLinks))
   .catch(err => console.log(err))
 
 function showLinks(card) {
-  const el = document.createElement('a')
-  el.textContent = card.title
+  const el = document.createElement('div')
+  el.classList.add('cards')
+  el.innerHTML = `<h2> ${card.title} </h2>
+  <p class="question" > ${card.question} </p>
+  <p class="answer" > ${card.answer} </p>
+  <button> Show </button>
+`
   el.href = 'http://localhost:3333/cards/' + card.id
   el.style.display = 'block'
   document.body.appendChild(el)
+
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach(button => button.addEventListener('click', showCards))
 }
+function showCards() {
+  const questions = document.querySelectorAll('.question')
+  questions.forEach(question => question.classList.toggle('active'))
+  const answers = document.querySelectorAll('.answer')
+  answers.forEach(answer => answer.classList.toggle('active'))
+}
+
+// weitere Elemente erzeugen
+// div mit Classe
+// Karte mit Titel und border
+// Klasse interaktiv
+// show bei click auf Show sieht man den Titel und Question und Answer
+// Hide
+// toggle
